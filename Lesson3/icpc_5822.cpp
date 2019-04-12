339782cfbdcb1cfc2a642dc993a8063d2ea37700
Yakir <yakir.helets@gmail.com>
2019-04-12 20:03:06 +0300

    Improved solution for Distributing Ballot Boxes

--- Lesson3/icpc_5822.cpp
+++ Lesson3/icpc_5822.cpp
@@ -4,6 +4,41 @@
 #include <tuple>
 
 using namespace std;
+
+bool isValidSolution(vector<int> v, int sol, int box_num) {
+    int i=0;
+    while(box_num) {
+        v[i]-=sol;
+        if (v[i] <= 0) {
+            i = (i+1)%v.size();
+            if (i == 0) {
+                break;
+            }
+        }
+        box_num--;
+    }
+    if (box_num>0) {
+        return true;
+    } else {
+        return (v[i] <=0);
+    }
+
+}
+
+int binarySearch (vector<int> vec, int box_num, int low, int high) {
+    int sol = ((high+low)/2);
+    if (low >= high) {
+        return sol;
+    }
+
+    vector<int> v(vec);
+    if (isValidSolution(v, sol, box_num)) {
+        return binarySearch(vec, box_num, low, sol);
+    } else {
+        return binarySearch(vec, box_num, sol+1, high);
+    }
+}
+
 int main ()
 {
     int cities;
@@ -17,31 +52,22 @@ int main ()
         int pop;
 
         if (cities == -1) break;
-        vector< tuple<int, int, int> > q(cities);
+        vector<int> q(0);
 
 
-        boxes -= cities;
+        //boxes -= cities;
+        int high = 0;
+        int low = 1;
         while (cities--)
         {
             cin >> pop;
-
-            q.push_back(tuple<int, int, int>(pop, 1, pop));
+            if (pop > high) {
+                high = pop;
+            }
+            q.push_back(pop);
         }
-
-        make_heap(q.begin(), q.end());
-
-        while (boxes--)
-        {
-             tuple<int, int, int> max = q.front();
-             std::pop_heap (q.begin(),q.end()); 
-             q.pop_back();
-
-             tuple<int, int, int> tmp(get<2>(max) / (get<1>(max) + 1), (get<1>(max) + 1), (get<2>(max)));
-             q.push_back(tmp); std::push_heap (q.begin(),q.end());
-        }
-
-        cout << get<0>(q.front()) << endl;
-
+        int sol = binarySearch(q, boxes, low, high);
+        cout << sol << endl;
     }
     return 0;
 }
